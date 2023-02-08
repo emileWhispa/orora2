7a4d490a0e155b8d18d8ad5dd3f11f47998267cb
whispa <kwizeraemile125@gmail.com>
2023-02-08 20:19:26 +0200

    commit

--- lib/json/farm.dart
+++ lib/json/farm.dart
@@ -4,10 +4,18 @@ class Farm {
   int livestocks;
   String sector;
   String? picture;
+  String? provinceId;
+  String? districtId;
+  String? sectorId;
+  String? categoryId;
 
   Farm.fromJson(Map<String, dynamic> map)
       : id = map['farm_id'],
         name = map['farm_name'],
+        provinceId = map['province_id'],
+        districtId = map['district_id'],
+        sectorId = map['sector_id'],
+        categoryId = map['category_id'],
         picture = map['farm_profile_picture'] ?? "",
         livestocks = map['livestocks'] ?? 0,
         sector = map['sector_name'];
